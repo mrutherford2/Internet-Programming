44871d5cc2938cef1f34c7361841fd6a8be09707
mrutherford2 <mattrutherford@marionsoftware.com>
2015-03-22 16:28:06 -0400

    Updated user.php file and login credentials

--- assign5/app/data/user.php
+++ assign5/app/data/user.php
@@ -1,13 +1,14 @@
 <?php 
 	error_reporting(E_ALL);
  	ini_set('display_errors', 1);
-	$user=json_decode(file_get_contents('php://input'), true);  //get user from 
+	$user=json_decode(file_get_contents('php://input'));  //get user from 
 	$file = "logininfo.txt";
 	$contents = (string)file_get_contents($file);
 	$data = explode(',',$contents);
 	echo " ";
-	echo $user['username'];
-	if($data[0]==$user->username) {
+	var_dump($user->password);
+	var_dump($data[1]);
+	if($data[0]==$user->username && $data[1]==$user->password) {
          	session_start();
 		$_SESSION['user']=uniqid('ang_');
 		$_SESSION['username']=$user->username;
