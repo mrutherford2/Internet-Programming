a757e76e8f715b9e8ea92e648e1ba3e24be18c3f
Matt Rutherford <n00682994@unf.edu>
2016-05-04 19:43:59 -0400

    Update db.php

--- assign7/app/data/db.php
+++ assign7/app/data/db.php
@@ -1,5 +1,5 @@
 <?php
-$GLOBALS['DB'] = new PDO('mysql:host=localhost;dbname=marionso_assign6;charset=utf8', 'marionso_assign6', 'Kush420$');
+$GLOBALS['DB'] = new PDO('mysql:host=localhost;dbname=marionso_assign6;charset=utf8', 'marionso_assign6', 'asldkfjw2342xcvm');
 $GLOBALS['DB']->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $GLOBALS['DB']->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
